2e11a2917374ac1e9c06bf5aa3a2eb55a65ed0dc
anmelnik <melnikvanastasia@gmail.com>
2021-01-11 23:04:19 +0200

    render elements

--- stack-overflow/src/Home/HomePage.jsx
+++ stack-overflow/src/Home/HomePage.jsx
@@ -1,15 +1,29 @@
 import React from "react";
-import * as axios from 'axios';
+import ItemsHome from "./ItemsHome";
+// import * as axios from 'axios';
 
-
-let x = axios.get(`https://api.stackexchange.com/2.2/questions?order=asc&sort=activity&site=stackoverflow`)
-.then(resp => resp.json())
-
-
-const Home = (props) => {
-  console.log(x.items)
-  return (
-      
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: undefined,
+    };
+  }
+  componentDidMount() {
+    fetch(
+      `https://api.stackexchange.com/2.2/questions?order=asc&sort=activity&site=stackoverflow`
+    )
+      .then((resp) => resp.json())
+      .then((result) => {
+        // while(!this.state.data) 
+        this.setState({
+          data: result,
+        });
+        // console.log(this.state.data.items[0]);
+      });
+  }
+  render() {
+    return (
       <article id="mainArticle">
         <div className="filterQuestions">
           <p id="title">Top Questions</p>
@@ -24,29 +38,54 @@ const Home = (props) => {
           </div>
         </div>
         <div className="allQuestions">
-        <div className="item"> 
-          <div className="contFeedback">
-            <p>0</p>
-            <p>votes</p>
-          </div>
-          <div className="contFeedback">
-            <p>0</p>
-            <p>answers</p>
-          </div>
-          <div className="contFeedback">
-            <p>0</p>
-            <p>views</p>
-          </div>
-          <div className="linkQuestions">
+        <ItemsHome data={this.state.data.items}/>
 
-          {/* <p>{x.items}</p> */}
-          </div>
-          <div className="tags">
-          <p></p>
-          </div>
-          </div>
         </div>
       </article>
-    )
-    }
+    );
+  }
+}
+// const Home = (props) => {
+//   console.log(x)
+//   return (
+//     <article id="mainArticle">
+//         <div className="filterQuestions">
+//           <p id="title">Top Questions</p>
+//           <button id="btnAskQues">Ask Questions</button>
+//           <p className="allShowQuestions">
+//             <span id="countQue">2000</span> questions
+//           </p>
+//           <div className="btn-tags">
+//             <button>Activity</button>
+//             <button>Votes</button>
+//             <button>Creation</button>
+//           </div>
+//         </div>
+//         <div className="allQuestions">
+//         <div className="item">
+//           <div className="contFeedback">
+//             <p>0</p>
+//             <p>votes</p>
+//           </div>
+//           <div className="contFeedback">
+//             <p>0</p>
+//             <p>answers</p>
+//           </div>
+//           <div className="contFeedback">
+//             <p>0</p>
+//             <p>views</p>
+//           </div>
+//           <div className="linkQuestions">
+
+//           {/* <p>{x}</p> */}
+//           </div>
+//           <div className="tags">
+//           <p></p>
+//           </div>
+//           </div>
+//         </div>
+//       </article>
+//     )
+//     }
+
 export default Home;
